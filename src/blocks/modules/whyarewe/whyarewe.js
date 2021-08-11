2a3b3f6c2316cd5a68432d10cb853c22e773fad8
nulloneguy <nulloneguy@ya.ru>
2021-08-11 18:01:55 +0700

    add whyarewe block

---
+++ src/blocks/modules/whyarewe/whyarewe.js
@@ -0,0 +1,13 @@
+modules.define('whyarewe', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
